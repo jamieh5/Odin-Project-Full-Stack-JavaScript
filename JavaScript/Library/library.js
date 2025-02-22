3c719b95b7dc67e0dd45df9ba18d4be0aedceb51
Jamie <hoyer307@gmail.com>
2025-02-22 12:15:54 +0100

    added some basic css and some assets to the project

--- JavaScript/Library/library.js
+++ JavaScript/Library/library.js
@@ -1,1 +1,22 @@
 // Task: https://www.theodinproject.com/lessons/node-path-javascript-library
+
+const modal = document.querySelector('.addBookModal')
+const addBook = document.querySelector('.addBook')
+
+addBook.addEventListener('click', function () {
+  modal.open = true
+})
+
+const myLibrary = []
+
+function Book(name, author, pages, read) {
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+const atomicHabits = new Book('Atomic Habits', 'James Clear', 346, 'not read')
+
+myLibrary.push(atomicHabits)
+console.log(myLibrary)
