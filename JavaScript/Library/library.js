8797f868cc240d979832d5b4a2fa20bb7619ad13
Jamie <hoyer307@gmail.com>
2025-02-22 13:04:12 +0100

    improved the css and started on the JavaScript

--- JavaScript/Library/library.js
+++ JavaScript/Library/library.js
@@ -2,6 +2,21 @@
 
 const modal = document.querySelector('.addBookModal')
 const addBook = document.querySelector('.addBook')
+const read = document.querySelector('.read')
+
+let bookRead = false
+
+read.addEventListener('click', function () {
+  if (bookRead == false) {
+    read.style.background = '#a7c957'
+    read.innerHTML = 'Read'
+    bookRead = true
+  } else if (bookRead == true) {
+    bookRead = false
+    read.style.background = '#bc4749'
+    read.innerHTML = 'Not Read'
+  }
+})
 
 addBook.addEventListener('click', function () {
   modal.open = true
@@ -18,5 +33,11 @@ function Book(name, author, pages, read) {
 
 const atomicHabits = new Book('Atomic Habits', 'James Clear', 346, 'not read')
 
-myLibrary.push(atomicHabits)
-console.log(myLibrary)
+function createCard(book) {
+  const card = document.createElement('div')
+  const title = document.createElement('h3')
+  const author = document.createElement('p')
+  const pages = document.createElement('p')
+  const hasRead = document.createElement('button')
+  const deleteBtn = document.createElement('img')
+}
