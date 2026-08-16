ea9a56497d5d9f831f12db386ce100be40089a61
Jamie <hoyer307@gmail.com>
2026-08-16 13:25:52 +0200

    Worked abit on the accordion component.

--- React/CV Application/src/components/Accordion.jsx
+++ React/CV Application/src/components/Accordion.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../styles/accordion.css";
+import arrow from "../styles/arrow.svg";
 
 import PersonalInfo from "./PersonalInfo.jsx";
 
@@ -9,11 +10,18 @@ function Accordion() {
   return (
     <div>
       <div className="item" onClick={() => setActiveAccordion(0)}>
-        Test
+        <p>Personal Info</p>
+        <img
+          src={arrow}
+          alt="arrow-icon"
+          className="arrow"
+          style={{
+            transform:
+              activeAccordion === 0 ? "rotate(270deg)" : "rotate(90deg)",
+          }}
+        />
       </div>
-      <div className={activeAccordion == 0 ? "active" : "hidden"}>
-        <PersonalInfo />
-      </div>
+      <div className={activeAccordion == 0 ? "active" : "hidden"}>...</div>
     </div>
   );
 }
